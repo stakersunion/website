bdd373ea8fe46c8c4149b08f3f13cb7b33ffe960
thame <thame@stakersunion.com>
2024-05-13 08:55:29 -0700

    Update Title component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Title } from '@/components'
 import { useSplitsMetadata, useSplitsBalance } from '@/utils/query/splits'
 
 const Home = () => {
@@ -9,48 +10,51 @@ const Home = () => {
   const { data: balance, isLoading: loadingBalance } = useSplitsBalance()
 
   return (
-    <div className={'container my-16 grid grid-cols-1 md:grid-cols-3 gap-4'}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Members</CardTitle>
-          <CardDescription>Number of active Stakers Union Members</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {loadingMetadata ? (
-            <Skeleton className={'h-20'} />
-          ) : (
-            <p className={'text-6xl font-bold'}>{metadata.recipients.length}</p>
-          )}
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Total Distributed</CardTitle>
-          <CardDescription>Funds Distributed to Stakers Union Members</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className={'text-6xl font-bold'}>0Ξ</p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Current Funds</CardTitle>
-          <CardDescription>Current Funds Pending Distribution</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {loadingBalance ? (
-            <Skeleton className={'h-20'} />
-          ) : (
-            <p className={'text-6xl font-bold'}>
-              {
-                balance?.activeBalances?.['0x0000000000000000000000000000000000000000']
-                  ?.formattedAmount
-              }
-              Ξ
-            </p>
-          )}
-        </CardContent>
-      </Card>
+    <div className={'container'}>
+      <Title>Dashboard</Title>
+      <div className={'my-6 grid grid-cols-1 md:grid-cols-3 gap-4'}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Members</CardTitle>
+            <CardDescription>Number of active Stakers Union Members</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {loadingMetadata ? (
+              <Skeleton className={'h-20'} />
+            ) : (
+              <p className={'text-6xl font-bold'}>{metadata.recipients.length}</p>
+            )}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Distributed</CardTitle>
+            <CardDescription>Funds Distributed to Stakers Union Members</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className={'text-6xl font-bold'}>0Ξ</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Current Funds</CardTitle>
+            <CardDescription>Current Funds Pending Distribution</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {loadingBalance ? (
+              <Skeleton className={'h-20'} />
+            ) : (
+              <p className={'text-6xl font-bold'}>
+                {
+                  balance?.activeBalances?.['0x0000000000000000000000000000000000000000']
+                    ?.formattedAmount
+                }
+                Ξ
+              </p>
+            )}
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import { cn } from '@/utils/shadcn'
 const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' })
 
 const Title = ({ children }) => {
-  return <h1 className={cn('flex flex-1 justify-center text-6xl my-12', bebas.className)}>{children}</h1>
+  return <h1 className={cn('flex flex-1 text-6xl my-6 md:mt-0', bebas.className)}>{children}</h1>
 }
 
 export default Title

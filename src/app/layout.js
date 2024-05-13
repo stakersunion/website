6b5d408c1be1b95cab42b1ce1882bd41ce439067
thame <thame@stakersunion.com>
2024-05-13 09:12:15 -0700

    Change main content background

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
               color={'#ffffff'}
               showSpinner={false}
             />
-            <div className={'flex min-h-screen w-full flex-col bg-muted/40'}>
+            <div className={'flex min-h-screen w-full flex-col bg-muted/20'}>
               <Sidebar />
               <div className={'flex flex-col sm:gap-4 sm:py-4 sm:pl-14'}>
                 <header

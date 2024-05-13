1cec723be14906d293a31eea8d2e79ec4a5751c0
thame <thame@stakersunion.com>
2024-05-13 09:45:12 -0700

    Addresses page

--- src/app/account/addresses/page.js
+++ src/app/account/addresses/page.js
@@ -1,8 +1,52 @@
+'use client'
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useUser } from '@/utils/query/user'
+
 const Addresses = () => {
+  const { data: user, isLoading, isError } = useUser()
+
+  if (isLoading) {
+    return <Skeleton className={'h-[300px]'} />
+  }
+
+  console.log(user)
   return (
-    <div>
-      <h1>Addresses</h1>
-    </div>
+    <Table>
+      <TableCaption>A list of your submitted addresses.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className={'w-2/3'}>Address</TableHead>
+          <TableHead>Type</TableHead>
+          <TableHead>Status</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {!user.addresses.length && (
+          <TableRow>
+            <TableCell colSpan={3}>No addresses found.</TableCell>
+          </TableRow>
+        )}
+        {user.addresses.map((address) => {
+          return (
+            <TableRow key={address.id}>
+              <TableCell>{address.address}</TableCell>
+              <TableCell>{address.type}</TableCell>
+              <TableCell>{address.status}</TableCell>
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
   )
 }
 

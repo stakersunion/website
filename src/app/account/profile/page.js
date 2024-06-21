b1262bae1cf6bb20cb962ae427a31b2f0b1ef42c
thame <tomfadial@gmail.com>
2024-06-21 10:09:36 -0700

    Temporarily remove submit

--- src/app/account/profile/page.js
+++ src/app/account/profile/page.js
@@ -1,34 +1,9 @@
 'use client'
 
-import Link from 'next/link'
 import { ProfileForm } from '@/components/user'
-import { Button } from '@/components/ui/button'
-import { useProfile } from '@/utils/query/user/profile'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
-import { routes } from '@/utils/routes'
 
 const Profile = () => {
-  const { data: profile } = useProfile()
-
-  return (
-    <ProfileForm
-      extraActions={
-        <Button
-          disabled={!profile?.name}
-          className={'ml-auto'}
-        >
-          <Link href={routes.account.children.addresses.path}>
-            Manage Addresses
-            <FontAwesomeIcon
-              icon={faArrowRight}
-              className={'ml-2'}
-            />
-          </Link>
-        </Button>
-      }
-    />
-  )
+  return <ProfileForm />
 }
 
 export default Profile

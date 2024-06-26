c7a6a75865827c0d2baf2dc0ae2ce51010a5721d
thame <tomfadial@gmail.com>
2024-06-26 11:31:08 -0700

    Update routes to account for webhook delays

--- src/app/api/user/verification/route.js
+++ src/app/api/user/verification/route.js
@@ -4,14 +4,24 @@ import connect from '@/utils/mongoose'
 import User from '@/models/user'
 
 export async function GET() {
-  const { id } = (await currentUser()) ?? {}
+  try {
+    const { id } = (await currentUser()) || {}
 
-  if (!id) {
-    return NextResponse.json({ error: 'Not logged in' }, { status: 401 })
-  } else {
+    if (!id) {
+      return NextResponse.json({ error: 'Not logged in' }, { status: 401 })
+    }
+
     await connect()
     const user = await User.findOne({ id })
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not yet created' }, { status: 404 })
+    }
+
     return NextResponse.json(user.verification, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
   }
 }
 

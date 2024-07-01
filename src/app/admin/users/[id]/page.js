84acf2c36e7831b460dabbdce602a59b4ab6e1ca
thame <tomfadial@gmail.com>
2024-07-01 09:59:22 -0700

    Move user page

---
+++ src/app/admin/users/[id]/page.js
@@ -0,0 +1,23 @@
+import { Separator } from '@/components/ui/separator'
+import { Profile, Verification, Addresses, Validators } from '@/components/admin/user'
+
+const User = ({ params }) => {
+  const { id } = params
+
+  return (
+    <div>
+      <h1 className={'text-2xl font-bold'}>User</h1>
+      <p className={'text-muted-foreground text-sm'}>{id}</p>
+      <Separator className={'my-6'} />
+      <div className={'flex flex-1 gap-4'}>
+        <Profile id={id} />
+        <Verification id={id} />
+      </div>
+      <div className={'my-6'}>
+        <Addresses id={id} />
+      </div>
+    </div>
+  )
+}
+
+export default User

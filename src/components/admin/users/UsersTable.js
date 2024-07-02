3fbc4c1f505b3ddb938fca986b39fb18e27e5530
thame <tomfadial@gmail.com>
2024-07-01 17:54:16 -0700

    Refetch user data

--- src/components/admin/users/UsersTable.js
+++ src/components/admin/users/UsersTable.js
@@ -1,21 +1,39 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { useUsers } from '@/utils/query/admin/users'
 import { Skeleton } from '@/components/ui/skeleton'
 import { DataTable, columns } from '@/components/admin/users/table'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLoader } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
 
 const UsersTable = () => {
-  const { data: users, isLoading } = useUsers()
+  const { data: users, isLoading, isRefetching, refetch } = useUsers()
 
   if (isLoading) {
     return <Skeleton className={'h-10'} />
   }
 
   return (
-    <DataTable
-      columns={columns}
-      data={users || []}
-    />
+    <>
+      <DataTable
+        columns={columns}
+        data={users || []}
+      />
+      <Button
+        onClick={refetch}
+        disabled={isRefetching}
+        className={'mt-4'}
+      >
+        {isRefetching && (
+          <FontAwesomeIcon
+            icon={faLoader}
+            className={'animate-spin mr-2'}
+          />
+        )}
+        Refresh
+      </Button>
+    </>
   )
 }
 

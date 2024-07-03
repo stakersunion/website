1398da8fac00e59e1ec2ad865346502d3bde03eb
thame <tomfadial@gmail.com>
2024-07-03 08:23:24 -0700

    Don't cache count

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import connect from '@/utils/mongoose'
 import User from '@/models/user'
 
+export const dynamic = 'force-dynamic'
+
 // Get Verified User Count
 export async function GET() {
   try {

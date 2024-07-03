32265cf916121d838b4689f5a0f978e6f7dc889e
thame <tomfadial@gmail.com>
2024-07-03 10:52:01 -0700

    Extract ICS creation

--- src/components/apply/CalendarFile.js
+++ src/components/apply/CalendarFile.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { createEvent } from 'ics'
+import createEvent from '@/utils/ics'
 import { useVerification } from '@/utils/query/user/verification'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar, faLoader, faWarning } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
@@ -9,50 +9,17 @@ const CalendarFile = () => {
   const { data: verification, isLoading: loadingVerification } = useVerification()
   const [error, setError] = useState(null)
 
-  const event = {
-    duration: { minutes: 30 },
-    title: 'Proof of Independent Operation',
-    description:
-      'You are scheduled to disable attestations for verification of independent operation of your validator.',
-    location: 'Ethererum Mainnet',
-    url: 'https://members.stakersunion.com',
-    status: 'CONFIRMED',
-    busyStatus: 'BUSY',
-    organizer: { name: 'Me', email: 'thame@stakersunion.com' },
-  }
-
-  const dateToICSFormat = () => {
-    let date = new Date(verification.independent.schedule)
-    return [
-      date.getFullYear(),
-      date.getMonth() + 1,
-      date.getDate(),
-      date.getHours(),
-      date.getMinutes(),
-      date.getSeconds(),
-    ]
-  }
-
   const handleDownload = () => {
     if (!verification.independent.schedule) {
       return setError('No schedule found')
     }
 
-    event.start = dateToICSFormat()
-
-    createEvent(event, (error, value) => {
-      if (error) {
-        setError(error)
-        return
-      }
-
-      const blob = new Blob([value], { type: 'text/calendar' })
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      a.href = url
-      a.download = 'event.ics'
-      a.click()
-      URL.revokeObjectURL(url)
+    createEvent({
+      time: verification.independent.schedule,
+      title: 'Proof of Independent Operation',
+      description:
+        'You are scheduled to disable attestations for verification of independent operation of your validator.',
+      setError,
     })
   }
 
---
+++ src/utils/ics.js
@@ -0,0 +1,50 @@
+import { createEvent as createIcs } from 'ics'
+
+const createEvent = ({
+  time = Date.now(),
+  title = 'Stakers Union Event',
+  description = '',
+  setError = () => {},
+}) => {
+  let event = {
+    duration: { minutes: 30 },
+    title,
+    description,
+    location: 'Ethererum Mainnet',
+    url: 'https://members.stakersunion.com',
+    status: 'CONFIRMED',
+    busyStatus: 'BUSY',
+    organizer: { name: 'Thame from Stakers Union', email: 'thame@stakersunion.com' },
+  }
+
+  const dateToICSFormat = () => {
+    let date = new Date(time)
+    return [
+      date.getFullYear(),
+      date.getMonth() + 1,
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ]
+  }
+
+  event.start = dateToICSFormat()
+
+  createIcs(event, (error, value) => {
+    if (error) {
+      setError(error)
+      return
+    }
+
+    const blob = new Blob([value], { type: 'text/calendar' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'event.ics'
+    a.click()
+    URL.revokeObjectURL(url)
+  })
+}
+
+export default createEvent

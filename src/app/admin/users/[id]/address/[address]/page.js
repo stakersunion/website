b2e3b0bc79878eec3219ee6e6b6f09fccf10b7ba
thame <tomfadial@gmail.com>
2024-08-04 11:22:38 -0700

    Use back link
    Removed breadcrumbs

--- src/app/admin/users/[id]/address/[address]/page.js
+++ src/app/admin/users/[id]/address/[address]/page.js
@@ -1,12 +1,35 @@
 'use client'
 
+import Link from 'next/link'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { AddressForm } from '@/components/admin/user/addresses'
 import { Validators } from '@/components/admin/user/validators'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
+import { routes, getRoute } from '@/utils/routes'
 
 const Address = ({ params }) => {
+
   return (
     <div className={'flex flex-col gap-y-4'}>
+      <div className={'flex flex-1 flex-row items-center'}>
+        <Link
+          href={getRoute({
+            path: routes.admin.children.users.children.user.path,
+            params: { id: params.id },
+          })}
+        >
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            className={'text-xl text-primary py-4 pr-4'}
+          />
+        </Link>
+        <div className={'flex flex-col'}>
+          <h1 className={'text-2xl font-bold'}>User</h1>
+          <p className={'text-muted-foreground text-sm'}>{params.id}</p>
+        </div>
+      </div>
+
       <Card>
         <CardHeader>
           <CardTitle>Address</CardTitle>
--- src/app/admin/users/[id]/page.js
+++ src/app/admin/users/[id]/page.js
@@ -1,14 +1,28 @@
+import Link from 'next/link'
 import { Separator } from '@/components/ui/separator'
 import { Profile, Verification, Appeal } from '@/components/admin/user'
 import { AddressesTable } from '@/components/admin/user/addresses'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
+import { routes } from '@/utils/routes'
 
 const User = ({ params }) => {
   const { id } = params
 
   return (
     <div>
-      <h1 className={'text-2xl font-bold'}>User</h1>
-      <p className={'text-muted-foreground text-sm'}>{id}</p>
+      <div className={'flex flex-1 flex-row items-center'}>
+        <Link href={routes.admin.children.users.path}>
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            className={'text-xl text-primary py-4 pr-4'}
+          />
+        </Link>
+        <div className={'flex flex-col'}>
+          <h1 className={'text-2xl font-bold'}>User</h1>
+          <p className={'text-muted-foreground text-sm'}>{id}</p>
+        </div>
+      </div>
       <Separator className={'my-6'} />
       <div className={'grid grid-cols-1 md:grid-cols-3 gap-4'}>
         <div className={'md:col-span-1'}>

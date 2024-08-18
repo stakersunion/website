85f49bf4e04cfc058086b66c3cd568eb7b0e229d
thame <tomfadial@gmail.com>
2024-08-17 19:15:27 -0700

    Production caching

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import connect from '@/utils/mongoose'
 import News from '@/models/news'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req) {
   await connect()
   const news = await News.find()

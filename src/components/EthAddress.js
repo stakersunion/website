1072229118de8c6989f739ff3f0403055860ebfa
thame <tomfadial@gmail.com>
2024-08-18 21:24:36 -0700

    Set length to show

--- src/components/EthAddress.js
+++ src/components/EthAddress.js
@@ -3,23 +3,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClipboard } from '@awesome.me/kit-ebf6e3e7b8/icons/sharp/solid'
 import { cn } from '@/utils/shadcn'
 
-const EthAddress = ({ address, empty = 'Not set', clipboard = true, className }) => {
+const EthAddress = ({ address, empty = 'Not set', length = 5, clipboard = true, className }) => {
   const [copied, setCopied] = useState(false)
 
   if (!address) {
     return <p>{empty}</p>
   }
+
   const copyToClipboard = () => {
     navigator.clipboard.writeText(address)
     setCopied(true)
     setTimeout(() => setCopied(false), 2000)
   }
 
+  const shortenedAddress = () => {
+    if (length === 0) {
+      return address
+    } else {
+      return address.substring(0, length) + '...' + address.substring(address.length - length)
+    }
+  }
+
   return (
     <div className={'flex items-center'}>
-      <p className={cn('mr-2', className)}>
-        {address.substring(0, 5)}...{address.substring(address.length - 5)}
-      </p>
+      <p className={cn('mr-2', className)}>{shortenedAddress()}</p>
       {clipboard && (
         <FontAwesomeIcon
           icon={faClipboard}
